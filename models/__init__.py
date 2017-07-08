94801ea0e7305db3130ab2124b3d43fe3853e684
theo-l <theol.liang@gamil.com>
2017-07-08 20:50:26 -0300

    use relative import

--- models/__init__.py
+++ models/__init__.py
@@ -2,6 +2,6 @@
 # @Author: theo-l
 # @Date:   2017-06-28 20:38:33
 # @Last Modified by:   theo-l
-# @Last Modified time: 2017-07-08 20:46:47
+# @Last Modified time: 2017-07-08 20:50:07
 
-from base_models import BaseModel
+from .base_models import BaseModel
