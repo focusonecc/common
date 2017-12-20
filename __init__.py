7056694d96025a1af7df89c6d5d7386815180ca5
theo-l <theo-l.liang@gmail.com>
2017-12-20 15:55:56 +0800

    fixed conflict

--- __init__.py
+++ __init__.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 # @Author: theo-l
-<<<<<<< HEAD
 # @Date:   2017-09-08 09:44:22
 # @Last Modified by:   theo-l
 # @Last Modified time: 2017-09-08 15:52:06
@@ -9,10 +8,3 @@ import doctest
 __test__ = {
     'Doctest': doctest
 }
-=======
-# @Date:   2017-06-26 18:50:04
-# @Last Modified by:   theo-l
-# @Last Modified time: 2017-06-26 18:50:11
-
-
->>>>>>> a1c36b07fbdd3d2c91462ecec37ffa8a83176473
