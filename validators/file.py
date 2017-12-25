a25a54dc53575d2497bb0ff053b0d73fda26902f
theo-L <theol.liang@gmail.com>
2017-12-25 11:00:55 +0800

    added more audio extension support

--- validators/file.py
+++ validators/file.py
@@ -2,8 +2,9 @@ import os
 from django.core.exceptions import ValidationError
 
 valid_image_extensions = ['.jpg', '.jpeg', '.png', '.gif']
-valid_audio_extensions = ['.mp3', '.wav',
-                          '.3gp', '.aa', '.wav', '.rm', '.webm']
+valid_audio_extensions = ['.mp3', '.wav', '.aac', '.ac3', '.aiff',
+                          '.m4a', '.mp2', '.ogg', '.ra', '.au', '.wma',
+                          '.mka', '.flac', '.3gp', '.aa', '.rm', '.webm']
 
 valid_video_extensions = ['.mp4', '.rmbv', '.mkv', '.webm', '.flv', '.vob',
                           '.ogv', '.ogg', '.drc', '.gif', '.gifv', '.mng',
@@ -13,19 +14,19 @@ valid_video_extensions = ['.mp4', '.rmbv', '.mkv', '.webm', '.flv', '.vob',
 
 
 def validate_file_extension(value, valid_extensions=None):
-    valid_extensions = valid_extensions or []
-    ext = os.path.splitext(value.name)[1]
-    if not ext.lower() in valid_extensions:
-        raise ValidationError('Unsupported audio file extension.')
+  valid_extensions = valid_extensions or []
+  ext = os.path.splitext(value.name)[1]
+  if not ext.lower() in valid_extensions:
+    raise ValidationError('Unsupported audio file extension.')
 
 
 def validate_image_extensions(value, valid_extensions=valid_image_extensions):
-    return validate_file_extension(value, valid_extensions=valid_extensions)
+  return validate_file_extension(value, valid_extensions=valid_extensions)
 
 
 def validate_audio_extensions(value, valid_extensions=valid_audio_extensions):
-    return validate_file_extension(value, valid_extensions=valid_extensions)
+  return validate_file_extension(value, valid_extensions=valid_extensions)
 
 
 def validate_video_extensions(value, valid_extensions=valid_video_extensions):
-    return validate_file_extension(value, valid_extensions=valid_extensions)
+  return validate_file_extension(value, valid_extensions=valid_extensions)
